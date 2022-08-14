8dbc9f4fcc7bbdfcfb0bf56317bd1dbc2bc76ae2
dcsuh <daniel.c.suh@gmail.com>
2022-08-14 15:57:39 -0400

    make new fig comparing body size and spore yield

--- scripts/spore_yield_body_size.R
+++ scripts/spore_yield_body_size.R
@@ -14,17 +14,9 @@ spores_length <- left_join(length, spores, by = c("temp", "resource", "species")
 spores_length %>% filter(temp %in% const_temp & species =="D") %>% 
   ggplot(.,aes(x=mean_length, y=log_mean_yield, color = temp)) + 
   geom_point() +
-  geom_errorbar(aes(ymin = log_mean_yield-log_se, ymax = log_mean_yield+log_se)) +
+  geom_errorbar(aes(ymin = log_mean_yield-log_se, ymax = log_mean_yield+log_se, width = 0.01)) +
   geom_errorbarh(aes(xmin = mean_length-se.x, xmax = mean_length+se.x)) +
-  scale_color_manual() +
-  labs(x = "Average Length (mm)", y = "ln(Average Spore Yield)")
-  
-
-length %>% filter(temp %in% const_temp & species =="D") %>% 
-  ggplot(., aes(x=resource, y = mean_length, color = inf_status)) + 
-  geom_point(aes(), position = position_dodge(width = 0.5)) + 
-  geom_errorbar(aes(ymin = mean_length-se, ymax = mean_length+se), position = position_dodge(width = 0.5)) + 
-  facet_wrap(vars(temp)) +
-  scale_color_manual(values = c("red", "blue")) + 
-  labs(y = "Average Length (mm)", x = "Resource Concentration mgC/L", color = "Infection\nStatus" ) +
+  scale_color_manual(values = c("#619CFF", "#00BA38", "#F8766D")) +
+  labs(x = "Average Length (mm)", y = "ln(Average Spore Yield)") +
   theme_bw()
+  
