a7c401bb02742668a49e5df71c24e7839c0cd8a3
dcsuh <daniel.c.suh@gmail.com>
2022-08-16 11:44:23 -0400

    update color

--- scripts/little_r.R
+++ scripts/little_r.R
@@ -10,9 +10,9 @@ little_r_fig <- lt.summary_factors %>% filter(species=="daphnia", temp_var == 0)
   ggplot(.,aes(x=as.factor(resource),y=S.r,color=inf_status)) + 
   geom_point(aes(), position = position_dodge(width = 0.5)) + 
   geom_linerange(aes(ymax=S.r.975, ymin=S.r.025), position = position_dodge(width = 0.5)) +
-  scale_color_manual(values = c("blue", "red")) + 
+  scale_color_manual(values = c("red", "blue")) + 
   facet_wrap(. ~ temp_id, nrow = 1) + 
   labs(y = "Host net reproductive rate", x = "Resource Concentration mgC/L", color = "Infection\nStatus" ) +
-  theme_bw()
+  proj_theme
 
 ggsave("little_r.png", little_r_fig, width = outwidth[1], height = outwidth[1]/golden, unit = "in", path = here("figures"))
