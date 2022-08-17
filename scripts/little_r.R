f325d13045003ff4cf6917f664f664ed5c959c2e
dcsuh <daniel.c.suh@gmail.com>
2022-08-16 23:02:04 -0400

    add versions of figs

--- scripts/little_r.R
+++ scripts/little_r.R
@@ -16,3 +16,15 @@ little_r_fig <- lt.summary_factors %>% filter(species=="daphnia", temp_var == 0)
   proj_theme
 
 ggsave("little_r.png", little_r_fig, width = outwidth[1], height = outwidth[1]/golden, unit = "in", path = here("figures"))
+
+little_r_I <- lt.summary_factors %>% filter(species=="daphnia", temp_var == 0, inf_status=="I") %>% 
+  ggplot(.,aes(x=as.factor(resource),y=S.r,color=inf_status)) + 
+  geom_point(aes(), position = position_dodge(width = 0.5)) + 
+  geom_linerange(aes(ymax=S.r.975, ymin=S.r.025), position = position_dodge(width = 0.5)) +
+  scale_color_manual(values = c("red", "blue")) + 
+  facet_wrap(. ~ temp_id, nrow = 1) + 
+  labs(y = "Host net reproductive rate", x = "Resource Concentration mgC/L", color = "Infection\nStatus" ) +
+  proj_theme + 
+  theme(legend.position = "none")
+
+ggsave("little_r_I.png", little_r_I, width = outwidth[1], height = outwidth[1]/golden, unit = "in", path = here("figures"))
