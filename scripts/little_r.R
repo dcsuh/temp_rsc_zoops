a33fd06e08df57609be9732000de02ced6e887f2
dcsuh <daniel.c.suh@gmail.com>
2023-03-02 14:43:59 -0500

    fix theme

--- scripts/little_r.R
+++ scripts/little_r.R
@@ -12,8 +12,7 @@ little_r_fig <- lt.summary_factors %>% filter(species=="daphnia", temp_var == 0)
   geom_linerange(aes(ymax=S.r.975, ymin=S.r.025), position = position_dodge(width = 0.5)) +
   scale_color_manual(values = c("red", "blue")) + 
   facet_wrap(. ~ temp_id, nrow = 1) + 
-  labs(y = "Host net reproductive rate", x = "Resource Concentration mgC/L", color = "Infection\nStatus" ) +
-  proj_theme
+  labs(y = "Host net reproductive rate", x = "Resource Concentration mgC/L", color = "Infection\nStatus" )
 
 ggsave("little_r.png", little_r_fig, width = outwidth[1], height = outwidth[1]/golden, unit = "in", path = here("figures"))
 
@@ -23,8 +22,7 @@ little_r_I <- lt.summary_factors %>% filter(species=="daphnia", temp_var == 0) %
   geom_linerange(aes(ymax=S.r.975, ymin=S.r.025), position = position_dodge(width = 0.5)) +
   scale_color_manual(values = c("red", "white")) + 
   facet_wrap(. ~ temp_id, nrow = 1) + 
-  labs(y = "Host net reproductive rate", x = "Resource Concentration mgC/L", color = "Infection\nStatus" ) +
-  proj_theme
+  labs(y = "Host net reproductive rate", x = "Resource Concentration mgC/L", color = "Infection\nStatus" )
 
 ggsave("little_r_I.png", little_r_I, width = outwidth[1], height = outwidth[1]/golden, unit = "in", path = here("figures"))
 
@@ -34,8 +32,7 @@ little_r_U <- lt.summary_factors %>% filter(species=="daphnia", temp_var == 0) %
   geom_linerange(aes(ymax=S.r.975, ymin=S.r.025), position = position_dodge(width = 0.5)) +
   scale_color_manual(values = c("white", "blue")) + 
   facet_wrap(. ~ temp_id, nrow = 1) + 
-  labs(y = "Host net reproductive rate", x = "Resource Concentration mgC/L", color = "Infection\nStatus" ) +
-  proj_theme
+  labs(y = "Host net reproductive rate", x = "Resource Concentration mgC/L", color = "Infection\nStatus" )
 
 ggsave("little_r_U.png", little_r_U, width = outwidth[1], height = outwidth[1]/golden, unit = "in", path = here("figures"))
 
