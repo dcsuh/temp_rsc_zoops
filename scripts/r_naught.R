a46f35398e6a6955b6a85b61e3122b5c2b09f70c
dcsuh <daniel.c.suh@gmail.com>
2023-03-24 10:32:37 -0400

    make r naught plot

--- scripts/r_naught.R
+++ scripts/r_naught.R
@@ -6,10 +6,11 @@ source(here("base","src.R"))
 lt_full <- readRDS(here("processed_data", "lt_full_summary.rds"))
 
 
-lt.summary_factors %>% filter(species=="D", temp_id %in% const_temp) %>% 
-  ggplot(.,aes(x=trt, y=S.R_naught)) + 
+lt_full %>% filter(species=="D", temp_id %in% const_temp) %>% 
+  ggplot(.,aes(x=as.factor(resource), y=S.R_naught)) + 
   geom_point() + 
   geom_linerange(aes(ymin=S.R_naught.025, ymax=S.R_naught.975)) + 
-  theme(axis.text.x = element_text(angle=90)) + 
+  facet_wrap(. ~ temp_id, nrow = 1) + 
+  labs(y = "R0", x = "Resource Concentration mgC/L") + 
   geom_hline(yintercept=1)
 
