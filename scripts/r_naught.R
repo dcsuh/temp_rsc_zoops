e1076cd7eb0cfacdde322c9cb937acf1b270d86e
dcsuh <daniel.c.suh@gmail.com>
2023-03-30 13:21:33 -0400

    add color

--- scripts/r_naught.R
+++ scripts/r_naught.R
@@ -8,9 +8,11 @@ lt_full <- readRDS(here("processed_data", "lt_full_summary.rds"))
 
 
 r_naught_fig <- lt_full %>% filter(species=="D", temp_var == 0) %>% 
-  ggplot(.,aes(x=as.factor(resource),y=S.R_naught)) + 
+  ggplot(.,aes(x=as.factor(resource),y=S.R_naught, color=temp_id)) + 
   geom_point(aes(), position = position_dodge(width = 0.5)) + 
   geom_linerange(aes(ymin=S.R_naught.025, ymax=S.R_naught.975)) + 
+  scale_color_manual(values = c("#619CFF", "#00BA38", "#F8766D")) +
+  guides(color="none")+
   facet_wrap(. ~ temp_id, nrow = 1) + 
   labs(y = "R0", x = "Resource Concentration mgC/L" )
 
@@ -18,9 +20,10 @@ ggsave("R0.png", r_naught_fig, width = outwidth[1], height = outwidth[1]/golden,
 
 
 r_naught_fig_temp <- lt_full %>% filter(species=="D", temp_var == 0) %>% 
-  ggplot(.,aes(x=as.factor(temp_id),y=S.R_naught)) + 
+  ggplot(.,aes(x=as.factor(temp_id),y=S.R_naught, color=temp_id)) + 
   geom_point(aes(), position = position_dodge(width = 0.5)) + 
   geom_linerange(aes(ymin=S.R_naught.025, ymax=S.R_naught.975)) + 
+  scale_color_manual(values = c("#619CFF", "#00BA38", "#F8766D")) + 
   facet_wrap(. ~ as.factor(resource), nrow = 1) + 
   labs(y = "R0", x = "Temp (C)" )
 
