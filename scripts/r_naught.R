664ec5fae198a0c3470810387a3b9714b527a1df
dcsuh <daniel.c.suh@gmail.com>
2023-03-30 13:01:11 -0400

    make r naught fig

--- scripts/r_naught.R
+++ scripts/r_naught.R
@@ -14,6 +14,13 @@ r_naught_fig <- lt_full %>% filter(species=="D", temp_id %in% const_temp) %>%
   labs(y = "R0", x = "Resource Concentration mgC/L") + 
   geom_hline(yintercept=1)
 
-ggsave("R0.png", r_naught_fig, width = outwidth[1], height = outwidth[1]/golden, unit = "in", path = here("figures"))
+r_naught_fig <- lt_full %>% filter(species=="D", temp_var == 0) %>% 
+  ggplot(.,aes(x=as.factor(resource),y=S.R_naught)) + 
+  geom_point(aes(), position = position_dodge(width = 0.5)) + 
+  geom_linerange(aes(ymin=S.R_naught.025, ymax=S.R_naught.975)) + 
+  facet_wrap(. ~ temp_id, nrow = 1) + 
+  labs(y = "R0", x = "Resource Concentration mgC/L" )
+
+#ggsave("R0.png", r_naught_fig, width = outwidth[1], height = outwidth[1]/golden, unit = "in", path = here("figures"))
 
 
