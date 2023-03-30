777a7582c2aaff116a54a2fa4c11cbd37c3ed05d
dcsuh <daniel.c.suh@gmail.com>
2023-03-30 13:03:09 -0400

    knit it

--- scripts/r_naught.R
+++ scripts/r_naught.R
@@ -6,13 +6,6 @@ source(here("base","src.R"))
 lt_full <- readRDS(here("processed_data", "lt_full_summary.rds"))
 
 
-r_naught_fig <- lt_full %>% filter(species=="D", temp_id %in% const_temp) %>% 
-  ggplot(.,aes(x=as.factor(resource), y=S.R_naught)) + 
-  geom_point() + 
-  geom_linerange(aes(ymin=S.R_naught.025, ymax=S.R_naught.975)) + 
-  facet_wrap(. ~ temp_id, nrow = 1) + 
-  labs(y = "R0", x = "Resource Concentration mgC/L") + 
-  geom_hline(yintercept=1)
 
 r_naught_fig <- lt_full %>% filter(species=="D", temp_var == 0) %>% 
   ggplot(.,aes(x=as.factor(resource),y=S.R_naught)) + 
@@ -21,6 +14,6 @@ r_naught_fig <- lt_full %>% filter(species=="D", temp_var == 0) %>%
   facet_wrap(. ~ temp_id, nrow = 1) + 
   labs(y = "R0", x = "Resource Concentration mgC/L" )
 
-#ggsave("R0.png", r_naught_fig, width = outwidth[1], height = outwidth[1]/golden, unit = "in", path = here("figures"))
+ggsave("R0.png", r_naught_fig, width = outwidth[1], height = outwidth[1]/golden, unit = "in", path = here("figures"))
 
 
