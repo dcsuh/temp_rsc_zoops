bdadb844e17fa71df6aca934f4a206c4a5a36cc1
dcsuh <daniel.c.suh@gmail.com>
2023-03-31 13:38:42 -0400

    made temporary fig

--- scripts/prob_infection.R
+++ scripts/prob_infection.R
@@ -11,8 +11,7 @@ prob_inf_fig <- prevalence %>% filter(temp %in% const_temp & species =="D") %>%
   geom_bar(stat = "identity", aes(fill = temp), width = 0.5, position = position_dodge(width = 0.5), color = "black") +
   geom_linerange(aes(ymax=conf$upper, ymin=conf$lower), position = position_dodge(width = 0.5), color = "black") +
   scale_fill_manual(values = c("#619CFF", "#00BA38", "#F8766D")) + 
-  labs(y = "Probability of Infection", x = "Resource Concentration (mgC/L)", fill = "Temperature") + 
-  proj_theme
+  labs(y = "Probability of Infection", x = "Resource Concentration (mgC/L)", fill = "Temperature")
 
 ggsave("prob_inf.png", prob_inf_fig, width = outwidth[1], height = outwidth[1]/golden, unit = "in", path = here("figures"))
 
