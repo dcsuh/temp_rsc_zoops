393590fd307120118bf2cc25b5595b4e62475dec
dcsuh <daniel.c.suh@gmail.com>
2023-08-16 13:25:11 -0700

    Create susc_fora_bootstrap.R

---
+++ susc_fora_bootstrap.R
@@ -0,0 +1,9 @@
+#Daniel Suh
+#August 16, 2023
+
+#estimate per-spore susceptibility using infection and foraging rate data
+#beta can be estimaed using infection data from a life table infection assay
+#foraging rate is measured separately
+#beta is per-spore susceptibility * foraging rate
+#so if we can estimate beta, then we can tease out per-spore susceptibility because we have foraging rate as well
+
