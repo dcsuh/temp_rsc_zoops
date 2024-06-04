e8ea3dce3b33160503e0cdabf8f223913b05e83e
dcsuh <daniel.c.suh@gmail.com>
2024-06-04 09:45:28 -0400

    update fits

--- multiple_mle.R
+++ multiple_mle.R
@@ -57,7 +57,10 @@ algae_plot_diff <- function(seq_data, var){
     scale_x_continuous(breaks = c(0.1, 0.5, 1.0)) + 
     guides(color = guide_legend(reverse=T)) + 
     #scale_color_manual(values = c("#619CFF", "#00BA38", "#F8766D"))
-    scale_color_manual(values = c("#FFC107", "#21918c", "#440154"))prev_plot <- function(seq_data, var){
+    scale_color_manual(values = c("#FFC107", "#21918c", "#440154"))
+}  
+  
+prev_plot <- function(seq_data, var){
   seq_data %>% filter(temp %in% const_temp) %>% 
     ggplot(.,aes(x=resource, 
                  y=!!sym(var), 
@@ -103,62 +106,62 @@ u_plot <- function(seq_data, var){
 # 
 # 
 # 
-# temp_range <- seq(15,25,by=0.1)
-# resource_range <- seq(0.1,1,by=0.01)
-# 
-# seq_data <- tibble(temp=c(),
-#                    resourece=c())
-# 
-# for (i in 1:length(temp_range)){
-#   tmp <- tibble(temp = temp_range[i],
-#                 resource = seq(0.1,1,by=0.01))
-#   seq_data %<>% bind_rows(., tmp)
-# }
-# 
-# 
-# lm(mm ~ resource + temp, data = data)
-# 
-# length_coef <- coef(lm(mm ~ resource + temp, data = data)) #lengths from foraging rate experiment
-# #length_ceof <- coef(lm(mm ~ resource + as.numeric(temp_id), data = lengths)) #5 day lengths from life table
-# 
-# 
-# interpolate_length <- function(resource, temp){
-#   length <- length_coef[1] + resource*length_coef[2] + temp*length_coef[3]
-# }
-# 
-# seq_data %<>% mutate(length = mapply(interpolate_length, 
-#                                      resource = resource,
-#                                      temp = temp))
-# 
-# 
-# fora_summ <- data %>% 
-#   mutate(ID = treatment_ID) %>%
-#   group_by(ID) %>% 
-#   summarize(end_r_mean = mean(amt_rem)*fora_vol/1000,
-#             end_r_se = (sd(amt_rem)/sqrt(n()))*fora_vol/1000)
-# 
-# prevalence <- readRDS(here("processed_data", "prevalence.rds"))
-# prevalence %<>% filter(temp %in% c(15, 20, 25)) %>% filter(species=="D") %>% mutate(ID = paste(temp, resource, sep="_")) %>% dplyr::select(-c(temp, resource, species))
-# 
-# 
-# viz_summ <- left_join(fora_summ, prevalence)
-# viz_summ %<>% left_join(., data_summ)
-# viz_summ %<>% left_join(., length_summ)
-# 
-# 
-# seq_data %<>% left_join(., viz_summ, by=c("temp", "resource"))
-# 
-# 
-# r_end_mean <- 
-#   data %>% group_by(temp, resource) %>% 
-#   summarize(amt_rem_mean = mean(amt_rem),
-#             amt_rem_se = sd(amt_rem)/sqrt(n()),
-#             amt_consumed_mean = mean(amt_consumed),
-#             amt_consumed_se = sd(amt_consumed)/sqrt(n())) %>%
-#   ungroup()
-# 
-# seq_data %<>% left_join(., r_end_mean)
-# 
+temp_range <- seq(15,25,by=0.1)
+resource_range <- seq(0.1,1,by=0.01)
+
+seq_data <- tibble(temp=c(),
+                   resourece=c())
+
+for (i in 1:length(temp_range)){
+  tmp <- tibble(temp = temp_range[i],
+                resource = seq(0.1,1,by=0.01))
+  seq_data %<>% bind_rows(., tmp)
+}
+
+
+lm(mm ~ resource + temp, data = data)
+
+length_coef <- coef(lm(mm ~ resource + temp, data = data)) #lengths from foraging rate experiment
+#length_ceof <- coef(lm(mm ~ resource + as.numeric(temp_id), data = lengths)) #5 day lengths from life table
+
+
+interpolate_length <- function(resource, temp){
+  length <- length_coef[1] + resource*length_coef[2] + temp*length_coef[3]
+}
+
+seq_data %<>% mutate(length = mapply(interpolate_length,
+                                     resource = resource,
+                                     temp = temp))
+
+
+fora_summ <- data %>%
+  mutate(ID = treatment_ID) %>%
+  group_by(ID) %>%
+  summarize(end_r_mean = mean(amt_rem)*fora_vol/1000,
+            end_r_se = (sd(amt_rem)/sqrt(n()))*fora_vol/1000)
+
+prevalence <- readRDS(here("processed_data", "prevalence.rds"))
+prevalence %<>% filter(temp %in% c(15, 20, 25)) %>% filter(species=="D") %>% mutate(ID = paste(temp, resource, sep="_")) %>% dplyr::select(-c(temp, resource, species))
+
+
+viz_summ <- left_join(fora_summ, prevalence)
+viz_summ %<>% left_join(., data_summ)
+viz_summ %<>% left_join(., length_summ)
+
+
+seq_data %<>% left_join(., viz_summ, by=c("temp", "resource"))
+
+
+r_end_mean <-
+  data %>% group_by(temp, resource) %>%
+  summarize(amt_rem_mean = mean(amt_rem),
+            amt_rem_se = sd(amt_rem)/sqrt(n()),
+            amt_consumed_mean = mean(amt_consumed),
+            amt_consumed_se = sd(amt_consumed)/sqrt(n())) %>%
+  ungroup()
+
+seq_data %<>% left_join(., r_end_mean)
+
 # 
 # coef(est_1)
 # 
@@ -314,77 +317,78 @@ u_plot <- function(seq_data, var){
 
 
 
-#est_m4 <- readRDS(file = here("mle", "final", "m4_fit.rds"))
-est_m4_1 <- readRDS(file = here("model_fitting", "m4_1_fit.rds"))
-est_m4_1.1 <- readRDS(file = here("model_fitting", "m4_1.1_fit.rds"))
-est_m4_0.5 <- readRDS(file = here("model_fitting", "m4_0.5_fit.rds"))
+est_m4 <- readRDS(file = here("model_fitting", "m4_coef_log_fit.rds"))
+est_m4_1 <- readRDS(file = here("model_fitting", "m4_coef_fit.rds"))
+est_m4_1.1 <- readRDS(file = here("model_fitting", "m4_1.1_coef_fit.rds"))
+est_m4_0.5 <- readRDS(file = here("model_fitting", "m4_0.5_coef_fit.rds"))
 
+coef(est_m4)
 coef(est_m4_1)
 coef(est_m4_1.1)
 coef(est_m4_0.5)
 
-#getAIC(est_m4)
+getAIC(est_m4)
 getAIC(est_m4_1)
 getAIC(est_m4_1.1)
 getAIC(est_m4_0.5)
 
 
 
-m4_num_sol <- function(t, x, params){
-  R <- x[1]
-  S <- x[2]
-  I <- x[3]
-  Z <- x[4]
-  
-  with(as.list(params),{
-    dR <- -R*(S+I)*(f*exp(arr_t_f*(1/ref_t - 1/temp))*(length^gamma))/
-      (1+f*exp(arr_t_f*(1/ref_t - 1/temp))*(length^gamma)*h*R*exp(w*temp))
-    
-    dS <- -Z*S*u*exp(arr_t_u*(1/ref_t - 1/temp))*exp(resource*rho)*(f*exp(arr_t_f*(1/ref_t - 1/temp))*(length^gamma))/
-      (1+f*exp(arr_t_f*(1/ref_t - 1/temp))*(length^gamma)*h*R*exp(w*temp))
-    
-    dI <- Z*S*u*exp(arr_t_u*(1/ref_t - 1/temp))*exp(resource*rho)*(f*exp(arr_t_f*(1/ref_t - 1/temp))*(length^gamma))/
-      (1+f*exp(arr_t_f*(1/ref_t - 1/temp))*(length^gamma)*h*R*exp(w*temp))
-    
-    dZ <- -Z*(S+I)*(f*exp(arr_t_f*(1/ref_t - 1/temp))*(length^gamma))/
-      (1+f*exp(arr_t_f*(1/ref_t - 1/temp))*(length^gamma)*h*R*exp(w*temp))
-    res <- c(dR, dS, dI, dZ)
-    list(res)}
-  )
-}
-
-
-
-m4_sim <- function(R, time, f, u, length, gamma, Z, ref_t, arr_t_f, arr_t_u, h, temp, resource, w, rho){
-  times <- seq(0, time, by=0.01)
-  xstart <- c(R=R,
-              S=1,
-              I=0,
-              Z=Z)
-  params <- c(f=f,
-              u=u,
-              length = length,
-              gamma = gamma,
-              Z = Z,
-              ref_t = ref_t,
-              arr_t_f = arr_t_f,
-              arr_t_u = arr_t_u,
-              h = h,
-              temp = temp,
-              resource = resource,
-              w = w,
-              rho = rho)
-  output <- as.data.frame(lsoda(y=xstart, times, m4_num_sol, params))
-  
-  if(Z == 0) {
-    end_data <- slice_max(output, time)[,2]
-  }
-  else {
-    end_data <- slice_max(output, time)[,4]  
-  }
-  
-  return(end_data)
-}
+# m4_num_sol <- function(t, x, params){
+#   R <- x[1]
+#   S <- x[2]
+#   I <- x[3]
+#   Z <- x[4]
+#   
+#   with(as.list(params),{
+#     dR <- -R*(S+I)*(f*exp(arr_t_f*(1/ref_t - 1/temp))*(length^gamma))/
+#       (1+f*exp(arr_t_f*(1/ref_t - 1/temp))*(length^gamma)*h*R*exp(w*temp))
+#     
+#     dS <- -Z*S*u*exp(arr_t_u*(1/ref_t - 1/temp))*exp(resource*rho)*(f*exp(arr_t_f*(1/ref_t - 1/temp))*(length^gamma))/
+#       (1+f*exp(arr_t_f*(1/ref_t - 1/temp))*(length^gamma)*h*R*exp(w*temp))
+#     
+#     dI <- Z*S*u*exp(arr_t_u*(1/ref_t - 1/temp))*exp(resource*rho)*(f*exp(arr_t_f*(1/ref_t - 1/temp))*(length^gamma))/
+#       (1+f*exp(arr_t_f*(1/ref_t - 1/temp))*(length^gamma)*h*R*exp(w*temp))
+#     
+#     dZ <- -Z*(S+I)*(f*exp(arr_t_f*(1/ref_t - 1/temp))*(length^gamma))/
+#       (1+f*exp(arr_t_f*(1/ref_t - 1/temp))*(length^gamma)*h*R*exp(w*temp))
+#     res <- c(dR, dS, dI, dZ)
+#     list(res)}
+#   )
+# }
+# 
+# 
+# 
+# m4_sim <- function(R, time, f, u, length, gamma, Z, ref_t, arr_t_f, arr_t_u, h, temp, resource, w, rho){
+#   times <- seq(0, time, by=0.01)
+#   xstart <- c(R=R,
+#               S=1,
+#               I=0,
+#               Z=Z)
+#   params <- c(f=f,
+#               u=u,
+#               length = length,
+#               gamma = gamma,
+#               Z = Z,
+#               ref_t = ref_t,
+#               arr_t_f = arr_t_f,
+#               arr_t_u = arr_t_u,
+#               h = h,
+#               temp = temp,
+#               resource = resource,
+#               w = w,
+#               rho = rho)
+#   output <- as.data.frame(lsoda(y=xstart, times, m4_num_sol, params))
+#   
+#   if(Z == 0) {
+#     end_data <- slice_max(output, time)[,2]
+#   }
+#   else {
+#     end_data <- slice_max(output, time)[,4]  
+#   }
+#   
+#   return(end_data)
+# }
 
 
 
@@ -413,6 +417,70 @@ m1_sim_Z <- function(R, time, f, u, length, gamma, Z, ref_t, arr_t_f, h, temp){
 
 
 
+m4_coef <- coef(est_m4)
+m4_f <- as.numeric(m4_coef[1])
+m4_u <- as.numeric(m4_coef[2])/100000
+m4_arr <- as.numeric(m4_coef[3])
+m4_arr_u <- as.numeric(m4_coef[4])
+m4_h <- as.numeric(m4_coef[5])
+m4_w <- as.numeric(m4_coef[6])
+m4_rho <- as.numeric(m4_coef[7])
+
+
+seq_data %<>% mutate(m4_end = mapply(m4_sim, 
+                                       R=resource*life_vol/1000, 
+                                       time=1, 
+                                       f=m4_f/life_vol, 
+                                       u = m4_u*exp(m4_rho*resource), 
+                                       length=length, 
+                                       gamma=gamma, 
+                                       Z=200*life_vol,
+                                       ref_t = ref_t,
+                                       arr_t_f = m4_arr,
+                                       arr_t_u = m4_arr_u,
+                                       h = m4_h*exp(m4_w*temp),
+                                       temp = temp))
+                                     # ,
+                                     #   resource = resource,
+                                     #   w = m4_w,
+                                     #   rho = m4_rho))
+
+seq_data %<>% mutate(m4_rate = (m4_f*exp(m4_arr*(1/ref_t - 1/temp))*
+                                    (length^gamma))/
+                       (1+m4_f*exp(m4_arr*(1/ref_t - 1/temp))*
+                          (length^gamma)*
+                          m4_h*exp(m4_w*temp)*
+                          resource/1000),
+                     m4_u_star = m4_u*exp(m4_arr_u*(1/ref_t - 1/temp))*exp(m4_rho*resource))
+
+seq_data %<>% mutate(m4_sp = mapply(m1_sim_Z, 
+                                        R=resource*life_vol/1000, 
+                                        time=1, 
+                                        f=m4_f/life_vol, 
+                                        u = 0, 
+                                        length=length, 
+                                        gamma=gamma, 
+                                        Z=200*life_vol,
+                                        ref_t = ref_t,
+                                        arr_t_f = m4_arr,
+                                        h = m4_h*exp(m4_w*temp),
+                                        temp = temp))
+
+seq_data %<>% mutate(m4_R = mapply(m4_sim, 
+                                       R=resource*fora_vol/1000, 
+                                       time=1, 
+                                       f=m4_f/fora_vol, 
+                                       u = 0, 
+                                       length=length, 
+                                       gamma=gamma, 
+                                       Z=0,
+                                       ref_t = ref_t,
+                                       arr_t_f = m4_arr,
+                                       arr_t_u = m4_arr_u,
+                                       h = m4_h*exp(m4_w*temp),
+                                       temp = temp))
+
+
 
 
 m4_coef <- coef(est_m4_1)
@@ -429,18 +497,19 @@ seq_data %<>% mutate(m4_1_end = mapply(m4_sim,
                                        R=resource*life_vol/1000, 
                                        time=1, 
                                        f=m4_f/life_vol, 
-                                       u = m4_u, 
+                                       u = m4_u*exp(m4_rho*resource), 
                                        length=length, 
                                        gamma=gamma, 
                                        Z=200*life_vol,
                                        ref_t = ref_t,
                                        arr_t_f = m4_arr,
                                        arr_t_u = m4_arr_u,
-                                       h = m4_h,
-                                       temp = temp,
-                                       resource = resource,
-                                       w = m4_w,
-                                       rho = m4_rho))
+                                       h = m4_h*exp(m4_w*temp),
+                                       temp = temp))
+                                       # ,
+                                       # resource = resource,
+                                       # w = m4_w,
+                                       # rho = m4_rho))
 
 seq_data %<>% mutate(m4_1_rate = (m4_f*exp(m4_arr*(1/ref_t - 1/temp))*
                                     (length^gamma))/
@@ -466,25 +535,28 @@ seq_data %<>% mutate(m4_1_sp = mapply(m1_sim_Z,
                                     Z=200*life_vol,
                                     ref_t = ref_t,
                                     arr_t_f = m4_arr,
-                                    h = m4_h,
+                                    h = m4_h*exp(m4_w*temp),
                                     temp = temp))
 
 seq_data %<>% mutate(m4_1_R = mapply(m4_sim, 
-                                   R=resource*fora_vol/1000, 
-                                   time=mean_time, 
-                                   f=m4_f/fora_vol, 
-                                   u = 0, 
-                                   length=length, 
-                                   gamma=gamma, 
-                                   Z=0,
-                                   ref_t = ref_t,
-                                   arr_t_f = m4_arr,
-                                   arr_t_u = m4_arr_u,
-                                   h = m4_h,
-                                   temp = temp,
-                                   resource = resource,
-                                   w = m4_w,
-                                   rho = m4_rho))
+                                     R=resource*fora_vol/1000, 
+                                     time=1, 
+                                     f=m4_f/fora_vol, 
+                                     u = 0, 
+                                     length=length, 
+                                     gamma=gamma, 
+                                     Z=0,
+                                     ref_t = ref_t,
+                                     arr_t_f = m4_arr,
+                                     arr_t_u = m4_arr_u,
+                                     h = m4_h*exp(m4_w*temp),
+                                     temp = temp))
+                                   # ,
+                                   # resource = resource,
+                                   # w = m4_w,
+                                   # rho = m4_rho))
+
+
 
 
 prev_plot(seq_data, "m4_1_end")
@@ -498,7 +570,7 @@ seq_data %>%
 
 m4_coef <- coef(est_m4_1.1)
 m4_f <- as.numeric(m4_coef[1])
-m4_u <- as.numeric(m4_coef[2])
+m4_u <- as.numeric(m4_coef[2])/100000
 m4_arr <- as.numeric(m4_coef[3])
 m4_arr_u <- as.numeric(m4_coef[4])
 m4_h <- as.numeric(m4_coef[5])
@@ -542,23 +614,23 @@ seq_data %<>% mutate(m4_1.1_sp = mapply(m1_sim_Z,
                                     temp = temp))
 
 seq_data %<>% mutate(m4_1.1_R = mapply(m4_sim, 
-                                   R=resource*life_vol/1000, 
-                                   time=1, 
-                                   f=m4_f/life_vol, 
-                                   u = m4_u*exp(m4_rho*resource), 
-                                   length=length, 
-                                   gamma=gamma, 
-                                   Z=0,
-                                   ref_t = ref_t,
-                                   arr_t_f = m4_arr,
-                                   arr_t_u = m4_arr_u,
-                                   h = m4_h*exp(m4_w*temp),
-                                   temp = temp))
+                                       R=resource*fora_vol/1000, 
+                                       time=1, 
+                                       f=m4_f/fora_vol, 
+                                       u = 0, 
+                                       length=length, 
+                                       gamma=gamma, 
+                                       Z=0,
+                                       ref_t = ref_t,
+                                       arr_t_f = m4_arr,
+                                       arr_t_u = m4_arr_u,
+                                       h = m4_h*exp(m4_w*temp),
+                                       temp = temp))
 
 
 m4_coef <- coef(est_m4_0.5)
 m4_f <- as.numeric(m4_coef[1])
-m4_u <- as.numeric(m4_coef[2])
+m4_u <- as.numeric(m4_coef[2])/100000
 m4_arr <- as.numeric(m4_coef[3])
 m4_arr_u <- as.numeric(m4_coef[4])
 m4_h <- as.numeric(m4_coef[5])
@@ -601,18 +673,18 @@ seq_data %<>% mutate(m4_0.5_sp = mapply(m1_sim_Z,
                                     temp = temp))
 
 seq_data %<>% mutate(m4_0.5_R = mapply(m4_sim, 
-                                   R=resource*life_vol/1000, 
-                                   time=1, 
-                                   f=m4_f/life_vol, 
-                                   u = m4_u*exp(m4_rho*resource), 
-                                   length=length, 
-                                   gamma=gamma, 
-                                   Z=0,
-                                   ref_t = ref_t,
-                                   arr_t_f = m4_arr,
-                                   arr_t_u = m4_arr_u,
-                                   h = m4_h*exp(m4_w*temp),
-                                   temp = temp))
+                                       R=resource*fora_vol/1000, 
+                                       time=1, 
+                                       f=m4_f/fora_vol, 
+                                       u = 0, 
+                                       length=length, 
+                                       gamma=gamma, 
+                                       Z=0,
+                                       ref_t = ref_t,
+                                       arr_t_f = m4_arr,
+                                       arr_t_u = m4_arr_u,
+                                       h = m4_h*exp(m4_w*temp),
+                                       temp = temp))
 
 #f
 #seq_data %>% ggplot(., aes(x=m4_rate, y=m4_1_rate)) + geom_point() + geom_abline()
@@ -670,10 +742,10 @@ spores_plot(seq_data, "m4_0.5_sp")
 
 seq_data %<>%
   mutate(amt_diff = amt_init_mean-amt_rem_mean,
-         m4_diff = resource - m4_R/50*1000,
-         m4_1_diff = resource - m4_1_R/50*1000,
-         m4_1.1_diff = resource - m4_1.1_R/50*1000,
-         m4_0.5_diff = resource - m4_0.5_R/50*1000)
+         m4_diff = resource - m4_R/fora_vol*1000,
+         m4_1_diff = resource - m4_1_R/fora_vol*1000,
+         m4_1.1_diff = resource - m4_1.1_R/fora_vol*1000,
+         m4_0.5_diff = resource - m4_0.5_R/fora_vol*1000)
 
 
 
