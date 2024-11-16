81c47640542877500b469c5b0b4e46821af1c53f
dcsuh <daniel.c.suh@gmail.com>
2024-11-15 19:58:46 -0500

    foraging scripts

---
+++ 03_infection.R
@@ -0,0 +1,6 @@
+#Build transmission model
+#Daniel Suh
+
+library(here)
+
+source(here("base","src.R"))
--- base/fns.R
+++ base/fns.R
@@ -1,12 +1,7 @@
-## script for defining functions used elsewhere in project
+#local functions
+#Daniel Suh
 
-#calculate prevalence using threshold spore yield
-
-get_threshold <- function(threshold, data = mort, factors = treatment_factors){
-  data %<>% mutate(inf_adj = ifelse(spore_yield>threshold, 1, 0))
-  prev_adj <- data %>% drop_na(inf_adj) #remove observations with NA for inf_adj
-  prev_adj %<>% group_by(ID) %>% summarize(n = n(), prev = sum(inf_adj)/n()) 
-  prev_adj %<>% left_join(.,treatment_factors)
-  return(prev_adj)
+#get AIC from mle outputs
+getAIC <- function(model){
+  return(2*length(coef(model))-(2*summary(model)@m2logL/-2))
 }
-
